7850bbb3d7dceff6c586265669962a7b4f34366a
Dennis Your <lavashell@gmail.com>
2017-10-07 19:25:47 -0400

    Firebase Authentication (Google Emails only)

---
+++ app/src/main/java/ec601/aty/food_app/LoginActivity.java
@@ -0,0 +1,13 @@
+package ec601.aty.food_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
